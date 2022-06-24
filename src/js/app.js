c0bdd545e8b36ee96e114ccde5aeb4ad0ee5fb23
Jaimie de Vries <49305844+jaimiedevries@users.noreply.github.com>
2022-06-24 15:32:13 +0200

    Puntjes n.a.v. review Thijs

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,14 @@
+const PAUSE_TIMER_ATTRIBUTE = 'data-pause-timer';
+const RESUME_TIMER_ATTRIBUTE = 'data-resume-timer';
+const TIMER_CONTROLS = `<button class="small-btn" data-pause-timer>Pause</button><button class="small-btn" data-stop-timer>Stop</button>`;
+const TIMER_INPUT = '<input id="minutes" type="number" placeholder="Aantal minuten"> <button class="small-btn" data-set-timer>Set</button>';
+
 let intervalId;
 let totaleTijdInSeconden;
-let ingevoerdeMinuten;
 let timeContainer = document.querySelector('.time');
 let dropdown = document.querySelector('[data-dropdown]');
 let menuContainer = document.querySelector('[data-popup-content]');
-
-const timerControls = '<button class="small-btn" data-pause-timer>Pause</button><button class="small-btn" data-stop-timer>Stop</button>';
-const timerInput = '<input id="minutes" type="number" placeholder="Aantal minuten"> <button class="small-btn" data-set-timer>Set</button>';
+let pauze = false;
 
 function closePopup(){
    dropdown.classList.remove('active');
@@ -20,25 +22,28 @@ function leegInterval(){
 function startTimer() {
   if(!intervalId) {
     intervalId = setInterval(timer, 1000);
-  } else {
-    return;
   }
 }
 
+function replaceAttributeWithButtonText(attributeToRemove, attributeToReplace, text) {
+  let btn = document.querySelector(`[${attributeToRemove}]`);
+  if(!btn) return;
+
+  btn.removeAttribute(attributeToRemove);
+  btn.setAttribute(attributeToReplace, '');
+  btn.innerHTML = text;
+}
+
 function pauseTimer() {
-    leegInterval();
-    let btn = document.querySelector('[data-pause-timer]');
-    btn.removeAttribute('data-pause-timer');
-    btn.setAttribute('data-resume-timer', '');
-    btn.innerHTML = 'Play';
+  pauze = true;
+  leegInterval();
+  replaceAttributeWithButtonText(PAUSE_TIMER_ATTRIBUTE, RESUME_TIMER_ATTRIBUTE, 'Play');
 }
 
 function resumeTimer(){
-    startTimer();
-    let btn = document.querySelector('[data-resume-timer]');
-    btn.removeAttribute('data-resume-timer');
-    btn.setAttribute('data-pause-timer', '');
-    btn.innerHTML = 'Pause';
+  pauze = false;
+  startTimer();
+  replaceAttributeWithButtonText(RESUME_TIMER_ATTRIBUTE, PAUSE_TIMER_ATTRIBUTE, 'Pause');
 }
 
 function stopTimer() {
@@ -48,30 +53,30 @@ function stopTimer() {
     timeContainer.innerHTML= '00:00:00';
   }
   closePopup();
-  bepaalPopupContent();
 }
 
 function stelTimerIn() {
   let minutenInput = document.getElementById('minutes').value;
-  let minutenInNumber = Number(minutenInput); 
-  ingevoerdeMinuten = minutenInNumber;
-  totaleTijdInSeconden = ingevoerdeMinuten * 60;
+  let minutenInNumber = Number(minutenInput);
+  totaleTijdInSeconden = minutenInNumber * 60;
   closePopup();
   startTimer();
+}
+
+function formatPartialTime(input) {
+  return input < 10 ? `0${input}` : input.toString();
 }
 
 function timer(){
   let uren = Math.floor(totaleTijdInSeconden / 3600);
   let minuten = Math.floor((totaleTijdInSeconden - (uren * 3600)) / 60);
   let seconden = totaleTijdInSeconden % 60;
-  uren = uren < 10 ? '0' + uren : uren;
-  minuten = minuten < 10 ? '0' + minuten : minuten;
-  seconden = seconden < 10 ? '0' + seconden : seconden;
-  timeContainer.innerHTML = `${uren}:${minuten}:${seconden}`;
+  timeContainer.innerHTML = `${formatPartialTime(uren)}:${formatPartialTime(minuten)}:${formatPartialTime(seconden)}`;
   totaleTijdInSeconden--;
-  (totaleTijdInSeconden < 0) ? clearInterval() : false;
+  if (totaleTijdInSeconden <= 0) {
+    clearInterval();
+  }
 }
-
 
 function timerActies(e) {
   const isSetButton = e.target.matches('[data-set-timer]');
@@ -87,8 +92,6 @@ function timerActies(e) {
     resumeTimer();
   } else if (isStopButton){
     stopTimer();
-  } else {
-    return
   }
 }
 
@@ -102,26 +105,53 @@ document.addEventListener('click', e => {
     }
 
     document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
-        if (dropdown === currentDropdown) return
-        dropdown.classList.remove('active');
+      if(dropdown == currentDropdown) return;
+      dropdown.classList.remove('active');
     });
 
     bepaalPopupContent();
     document.addEventListener('click', timerActies);
 });
 
-function bepaalPopupContent (){
-    if (intervalId) {
-        menuContainer.innerHTML = timerControls;
-    } else {
-        menuContainer.innerHTML = timerInput;
+function bepaalPopupContent () {
+    menuContainer.innerHTML = intervalId || pauze ? TIMER_CONTROLS : TIMER_INPUT;
+    if(pauze) {
+      replaceAttributeWithButtonText(PAUSE_TIMER_ATTRIBUTE, RESUME_TIMER_ATTRIBUTE, 'Play');
     }
 }
 
 
 
 
+// ----- Elke btn zijn eigen afhandeling laten doen i.p.v. alle clicks op het document checken of het een van de knoppen is
 
+// document.querySelector('#drop-down-button').addEventListener('click', e => {
+//   console.log('foo');
+// });
+
+// document.querySelector('.drop-down-menu').addEventListener('click', timerActies);
+
+// document.querySelector('#pause-or-start-btn').addEventListener('click', e => {
+//   const isSetButton = e.target.matches('[data-set-timer]');
+//   const isPauseButton = e.target.matches('[data-pause-timer]');
+//   const isResumeButton = e.target.matches('[data-resume-timer]');
+
+//   if(isSetButton) {
+//     stelTimerIn();
+//   } else if (isPauseButton) {
+//     pauseTimer();
+//   } else if (isResumeButton) {
+//     resumeTimer();
+//   }
+// });
+
+// document.querySelector('#stop-btn').addEventListener('click', e => {
+//   if(intervalId) {
+//     stopTimer();
+//   }
+// });
+
+// ----- End
 
 
 
